93710b2876859fce852c1fd7177725dc426bf700
Ashish-wel <ashishnagda34@gmail.com>
2025-08-22 18:40:05 +0530

    Create task 2_math_module.py

---
+++ task 2_math_module.py
@@ -0,0 +1,17 @@
+import math
+
+num = float(input("Enter a number: "))
+
+sqrt_val = math.sqrt(num)
+log_val = math.log(num)
+sine_val = math.sin(num)
+
+print("Square root: {}".format(sqrt_val))
+print("Logarithm: {}".format(log_val))
+print("Sine : {}".format(sine_val))
+
+-- Output --
+Enter a number: 16
+Square root: 4.0
+Logarithm: 2.772588722239781
+Sine : -0.2879033166650653
